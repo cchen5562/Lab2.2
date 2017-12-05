dde2de298a82fdc33b562c3a4b9a7f88b68975db
tina <cchen5562@bths.edu>
2017-12-04 22:07:10 -0500

    i sleep

--- PeopleLab2.2/src/Runner.java
+++ PeopleLab2.2/src/Runner.java
@@ -1,22 +1,29 @@
-
+import java.util.Random;
 public class Runner 
 {
+	//fields
+	//used a random name generator		
+	//a pre-made list of first names
+	private static int height;
+	private static String[] firstNames = {"Jerry", "Gunner", "Adelyn", "Khloe", "Jovani", "Madeleine", "Asa", "Alberto", "Ibrahim", "Ainsley",				 				
+							"Kyleigh", "Marc", "Angeline", "Corey", "Macy", "Angelo", "Mcintyre", "Alaina", "Yamilet", "Carleigh",
+			 				"Jonathan", "Dayami", "Jax", "Yair", "Jamarcus", "Kaylah", "Francis", "Junior", "Angelica", "Jacqueline",
+			 				"Shyanne", "Jovanni", "Jayvon", "Nathalia"};
+	//a pre-made list of family names
+	private static String[] familyNames = {"Davis", "Mckenzie", "Harding", "Young", "Waller", "Quinn", "Bartlett", "Woodard", "Singleton", "Long",								
+							"Humphrey", "Heath", "Orozco", "Mcclure", "Cisneros", "Ball", "Bush", "Lynch", "Ortega", "Vasquez",
+							"Moore", "Mays", "Mccoy", "White", "Wells", "Fritz", "Rush", "Snow", "Moran", "Stephenson",
+							"Mcintyre", "Mora", "Cobb", "Duran"};
+	private static double weight;
+	private static String Subject;
+	private static String Title;
+	
+	
 	public static void main(String[] args)
 	{
-		//fields
-		//used a random name generator
-		//a pre-made list of first names
-		String[] firstNames = {"Jerry", "Gunner", "Adelyn", "Khloe", "Jovani", "Madeleine", "Asa", "Alberto", "Ibrahim", "Ainsley",
-				 						"Kyleigh", "Marc", "Angeline", "Corey", "Macy", "Angelo", "Mcintyre", "Alaina", "Yamilet", "Carleigh",
-				 						"Jonathan", "Dayami", "Jax", "Yair", "Jamarcus", "Kaylah", "Francis", "Junior", "Angelica", "Jacqueline",
-				 						"Shyanne", "Jovanni", "Jayvon", "Nathalia"};
-		//a pre-made list of family names
-		String[] familyNames = {"Davis", "Mckenzie", "Harding", "Young", "Waller", "Quinn", "Bartlett", "Woodard", "Singleton", "Long",
-					"Humphrey", "Heath", "Orozco", "Mcclure", "Cisneros", "Ball", "Bush", "Lynch", "Ortega", "Vasquez",
-					"Moore", "Mays", "Mccoy", "White", "Wells", "Fritz", "Rush", "Snow", "Moran", "Stephenson",
-					"Mcintyre", "Mora", "Cobb", "Duran"};
-		
-		Student[] s = new Student[34];
+		Person teacher = new Teacher("68", "Joyin", "Wong", "100", "Life", "Mx");	
+		Person[] students;
+		students = new Person[34];
 		
 		//for(int i = 0; i < s.length; i++) 
 		//{
@@ -24,13 +31,24 @@ public class Runner
 			//System.out.println(j + "." + s.length);
 			//j++;
 		//} 
-		
-		public Runner (Teacher Teacher, Student Student, Classroom Classroom)
-		{
-			super(Teacher, Student, Classroom); //must call super constructor first
-			this.Teacher = Teacher;
-			this.Student = Student;
-			this.Classroom = Classroom;
-		}
+
 	}
+	
+	public static Student ranStudent()      //random student generator
+	{
+		Random r = new Random();
+		height = r.nextInt(26) + 58;		//58 to 84 inches
+		String firstNames = r.firstNames[r.nextInt(firstNames.length())];
+		String familyNames = r.familyNames[r.nextInt(familyNames.length())];
+		weight = r.nextInt(100) + 90; 		//90 to 190 pounds
+		
+		
+	}
+	
+	public static int randGPA (double GPA) 
+		{
+		GPA [] = {2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.8, 3.9, 4.0}; //GPA 2.5-4.0
+		GPA = r.GPA
+	
+		}
 }
